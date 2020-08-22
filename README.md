2e5d885bc16f006aaa590f2983f76fbc5a80975f
prabhasak <prabhasa.94@gmail.com>
2020-08-22 05:51:01 -0500

    tensorboard and callbacks

--- README.md
+++ README.md
@@ -4,13 +4,15 @@ Masters-Thesis: Learning from demonstrations
 
 **Framework, langauge, OS:** Tensorflow 1.14, Python 3.7, Windows 10
 
+**Motivation:** The original Stable Baselines codebase includes a great set of features, 
+
 **Idea**: pick {env, algo} pair -> train RL (optimal policy) -> generate expert data (optimal expert) -> train GAIL (policy)
 
 **Thesis:** Autonomous UAV landing using human demonstrations alone (expert). Apply imitation learning methods on a custom environment built on [Microsoft AirSim 2.0](https://microsoft.github.io/AirSim/). Short video [here](https://www.youtube.com/watch?v=oj4y8GOq4gk&feature=youtu.be)
 
 Prerequisites
 -------------
-The implementation uses [Stable Baselines 2.10](https://stable-baselines.readthedocs.io/en/master/guide/install.html). Note: You will need to install the OpenMPI version to use ``GAIL``, ``TRPO`` algorithms. Please include the 'utils' folder from [here](https://github.com/araffin/rl-baselines-zoo) in your cloned Stable Baselines repo
+The implementation uses [Stable Baselines 2.10](https://stable-baselines.readthedocs.io/en/master/guide/install.html). Note: You will need to install the OpenMPI version to use ``GAIL``, ``TRPO`` algorithms. I have inlcuded 'utils' from [here](https://github.com/araffin/rl-baselines-zoo) to save hyperparameters into a dictionary
 
 AirSim: Some resources to [generate](https://microsoft.github.io/AirSim/build_windows/) custom binary files, modify [settings](https://microsoft.github.io/AirSim/settings/). Binaries for my thesis available [here](https://drive.google.com/drive/folders/1PFYkOlqb0DLcVoSsaSNGZVJif1VGeGuK?usp=sharing)
 
@@ -23,16 +25,34 @@ git clone https://github.com/prabhasak/masters-thesis.git
 
 # install required libraries and modules (recommended)
 pip install -r requirements.txt
+
 # MPI needed for TRPO, GAIL
 pip install stable-baselines[mpi]
 ```
 
+Add your CustomEnv ID details to ``env_list, env_success, episode_len`` ([examples](https://github.com/openai/gym/blob/master/gym/envs/__init__.py)), and your custom RL/IL algorithm to ``algo_list`` (You can generate a CustomEnv ID by [registering](https://medium.com/@apoddar573/making-your-own-custom-environment-in-gym-c3b65ff8cdaa) your CustomEnv on Gym. You can use the ``"airsim_env"`` folder above as reference)
+
 Usage
 -------------
-Add your CustomEnv ID to ``env_list``, reward to ``env_success``, and your custom RL/IL algorithm to ``algo_list`` (You can generate a CustomEnv ID by [registering](https://medium.com/@apoddar573/making-your-own-custom-environment-in-gym-c3b65ff8cdaa) your CustomEnv on Gym. You can use the ``"airsim_env"`` folder above as reference)
+The codebase contains **[Tensorboard](https://stable-baselines.readthedocs.io/en/master/guide/tensorboard.html)** and **[Callback](https://stable-baselines.readthedocs.io/en/master/guide/callbacks.html)** features, which help monitor performance during training. You can enable them with ``-tb`` and ``-check, -eval``. The callbacks are for [saving](https://stable-baselines.readthedocs.io/en/master/guide/callbacks.html#checkpointcallback) the model periodically (useful for [continual learning](https://stable-baselines.readthedocs.io/en/master/guide/examples.html#continual-learning) and to resume training) and [evaluating](https://stable-baselines.readthedocs.io/en/master/guide/callbacks.html#evalcallback) the model periodically (saves the best model throughout training). You can choose to save and evaluate just the best model with ``-best``
 
-**OpenAI Gym envs:** ``python imitation_learning_basic.py --env Pendulum-v0 --algo sac --seed 42 -rl -trl 3e5 -il -til 3e5 -params-RL gamma:0.995 timesteps_per_batch: 2048`` (exclude ``-rl`` if expert data is available)
+**OpenAI Gym envs:** ``python imitation_learning_basic.py --seed 42 --env Pendulum-v0 --algo sac -rl -trl 1e5 -il -til 3e5 -best -check -eval -tb -params-RL learning_starts:1000 -params-IL lam:0.9 vf_iters:10`` (``--device`` assumes single-GPU machine. Exclude ``-rl`` if expert data is available)
+
+Verify reproducibility: (i) 71/100 successful experts with mean(-145.37/-150.43), std(80.41/82.06), and (ii) 43/100 successful episodes on GAIL policy evaluation with mean(-227.61/-200.78), std(147.36/109.21)
 
 **AirSim env:** ``python imitation_learning_basic.py --env AirSim-v0 --algo sac --exp-id 1 --seed 42 -rl -trl 4e5 -il -til 1e6 -params-IL gamma:0.995`` (run this after opening binaries/Windows/Blocks.exe or binaries/Linux/Blocks.sh)
 
-Tuned hyperparameters are available on [Baselines Zoo](https://github.com/araffin/rl-baselines-zoo/tree/master/hyperparams). Please read ``description.txt`` for more info on the sub-folders
+Tuned hyperparameters (HPs) are available on [Baselines Zoo](https://github.com/araffin/rl-baselines-zoo/tree/master/hyperparams). Please read ``description.txt`` for more info on the sub-folders
+
+Note: For [deterministic evaluation](https://github.com/hill-a/stable-baselines/issues/929#issuecomment-655319112) of expert data, add ``deterministic=True`` [here](https://github.com/hill-a/stable-baselines/blob/master/stable_baselines/gail/dataset/record_expert.py#L120). To hide expert data info (keys, shape), you will have to comment [this](https://github.com/hill-a/stable-baselines/blob/master/stable_baselines/gail/dataset/record_expert.py#L173)
+
+Future Work
+-------------
+1. [Multiprocessing](https://stable-baselines.readthedocs.io/en/master/guide/vec_envs.html#subprocvecenv): to speed up training (observed 6x speedup on my PC)
+2. [HP tuning](https://stable-baselines.readthedocs.io/en/master/guide/rl_zoo.html): to find the best set of hyperparameters for an (environment-algorithm) pair
+3. [VecNormalize](https://stable-baselines.readthedocs.io/en/master/guide/vec_envs.html#vecnormalize): for normalizing data (useful for MuJoCo environments)
+4. [Monitor](https://stable-baselines.readthedocs.io/en/master/common/monitor.html): for recording more internal state information
+5a. Comparing consecutive runs of the experiment and picking the best model
+5b. Loading tuned HPs and passing arguments to custom environments
+
+I plan to release a separate repo for all of this once my reasearch is done!
--- imitation_learning_basic.py
+++ imitation_learning_basic.py
@@ -1,7 +1,12 @@
-"""A basic implementation of Generative Adversarial Imitation Learning using Stable Baselines 2.10.
+"""Benchmark reinforcement learning (RL) and imitation Learning (GAIL) algorithms from Stable Baselines 2.10.
+Author: Prabhasa Kalkur
 
-- Author: Prabhasa Kalkur
-- Note: choose {env, RL algo, training time, hyperparameters} from cmd line, # policy evaluations needed (line 94), # expert trajectories generated & used (line 101, 112)
+- Note 1.1: choose {env, RL algo, training times, hyperparameters, } as cmd line arguments
+- Note 1.2: changeable numbers in the program:
+            number of episodes used for policy evaluation after training = 100 (line 146)
+            callback model saving and evaluation = every 100, 300 episodes for RL, IL (168, 205)
+            number of expert trajectories generated & used = 100, 10 (line 186, 195)
+- Note 2: Things you can add on top: Multiprocessing, Monitor, VecNormalize, HP tuning, pass CustomEnv kwargs
 """
 
 import os
@@ -12,39 +17,40 @@ import warnings
 import numpy as np
 import tensorflow as tf
 
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
 #Install Stable Baselines 2.10 (https://stable-baselines.readthedocs.io/en/master/guide/install.html)
 from stable_baselines import GAIL, TRPO, SAC, DQN, PPO2, A2C, DDPG, ACER, ACKTR, HER, TD3
+from stable_baselines.common.callbacks import CheckpointCallback, EvalCallback
 from stable_baselines.gail import ExpertDataset, generate_expert_traj
 from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines.common import set_global_seeds
-from stable_baselines.utils.utils import StoreDict
+from utils import StoreDict
+from shutil import copy
 
 # Install AirSim 2.0 (https://pypi.org/project/airsim/ and https://microsoft.github.io/AirSim/build_windows/)
 from airsim_env.envs.airsim_env_0 import AirSim
 
-# numpy warnings because of tensorflow
-warnings.filterwarnings("ignore", category=FutureWarning, module='tensorflow')
-warnings.filterwarnings("ignore", category=UserWarning, module='gym')
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
-#Add your env and algo here. Define your reward criteria in env_success
+# Add your coustom env and algo details here. Reference: https://github.com/openai/gym/blob/master/gym/envs/__init__.py
 algo_list = {'sac': SAC, 'trpo': TRPO, 'gail': GAIL, 'dqn': DQN, 'ppo2': PPO2,
             'ddpg': DDPG, 'acer': ACER, 'acktr': ACKTR, 'her': HER, 'td3': TD3}
-env_list = ['Pendulum-v0', 'CartPole-v1', 'LunarLander-v2', 'LunarLanderContinuous-v2', 'BipedalWalker-v2', 'BipedalWalkerHardcore-v2',
-            'HalfCheetah-v2', 'Hopper-v2', 'Humanoid-v2', 'Ant-v2', 'Reacher-v2', 'Swimmer-v2', 'AirSim-v0'] #mujoco envs, needs license
-env_success = [-200, 475, 200, 200, 300, 300, 9000, 3000, 1000, 6000, 3.75, 360, 1000] #not exactly what OpenAI Gym defines
+env_list = ['Pendulum-v0', 'CartPole-v1', 'LunarLander-v2', 'LunarLanderContinuous-v2', 'MountainCarContinuous-v0', 'BipedalWalker-v3',
+            'HalfCheetah-v2', 'Hopper-v2', 'Humanoid-v2', 'Ant-v2', 'Reacher-v2', 'Swimmer-v2', 'AirSim-v0'] # mujoco envs need license
+env_success = [-200, 475, 200, 200, 90, 300, 4800, 3000, 1000, 6000, 3.75, 360, 1000] # OpenAI Gym requirements (Hopper should be 3800)
+episode_len = [200, 500, 400, 400, 999, 1600, 1000, 1000, 1000, 1000, 50, 1000, 100]
 
-def train(exp_id, env_id, env, algo, policy, seed, timesteps, hyperparams=None, dataset=None):
-    env_name = env_id[:-3]
-    if algo!='gail':
-        model = (algo_list[algo])(policy=policy, env=env, seed=seed, n_cpu_tf_sess=1, **hyperparams) #n_cpu_tf_sess=1 for deterministic CPU results
-        model.learn(total_timesteps=timesteps)
-        model.save("models/{}_{}_{}".format(env_name.lower(), algo.lower(), exp_id)) #e.g. trpo_hopper_0
-    elif algo=='gail':
-        model = (algo_list[algo])(policy=policy, env=env, seed=seed, expert_dataset=dataset, n_cpu_tf_sess=1, **hyperparams)
-        model.learn(total_timesteps=timesteps)
-        model.save("models/gail_{}_{}_{}".format(env_name.lower(), algo.lower(), exp_id)) #e.g. gail_trpo_hopper_0
+def train(mode, save_best_model, folder, env, algo, policy, seed, timesteps, callback = None, tensorboard_log=None, hyperparams=None, dataset=None):
+    if mode=='RL':
+        model = (algo_list[algo])(policy=policy, env=env, seed=seed, n_cpu_tf_sess=1, tensorboard_log=tensorboard_log, **hyperparams) #n_cpu_tf_sess=1 for deterministic CPU results
+        model.learn(total_timesteps=timesteps, callback = callback)
+        if not save_best_model:
+            model.save("models/{}_{}_{}".format(*folder)) #e.g. 0_trpo_hopper
+    elif mode=='IL':
+        model = (algo_list['gail'])(policy=policy, env=env, seed=seed, expert_dataset=dataset, n_cpu_tf_sess=1, tensorboard_log=tensorboard_log, **hyperparams)
+        model.learn(total_timesteps=timesteps, callback = callback)
+        if not save_best_model:
+            model.save("models/{}_{}_{}_gail".format(*folder)) #e.g. 0_trpo_hopper_gail
 
 def evaluate(mode, quantity, env_id, env, algo, n_eval_episodes):
     env_index = env_list.index(env_id)
@@ -70,6 +76,30 @@ def evaluate(mode, quantity, env_id, env, algo, n_eval_episodes):
         print('Suboptimal {}. Please try again...'.format(mode))
         sys.exit()
 
+def add_callback_path(mode_algo, mode_callback, folder):
+    if mode_algo=='RL':
+        eval_callback_path = "callbacks/{}/{}_{}_{}".format(mode_callback, *folder)
+    elif mode_algo=='IL':
+        eval_callback_path = "callbacks/{}/{}_{}_{}_gail".format(mode_callback, *folder)
+    make_dir(eval_callback_path)
+    return eval_callback_path
+
+def copy_best_model(mode, folder):
+    if mode=='RL':
+        best_model_path = "callbacks/evaluations/{}_{}_{}".format(*folder)
+        last_model_path = "models/{}_{}_{}".format(*folder)
+    elif mode=='IL':
+        best_model_path = "callbacks/evaluations/{}_{}_{}_gail".format(*folder)
+        last_model_path = "models/{}_{}_{}_gail".format(*folder)
+    copy(os.path.join(best_model_path,'best_model.zip'), "models/")
+    if os.path.exists(last_model_path+'.zip'):
+        os.remove(last_model_path+'.zip')
+    os.rename("models/best_model.zip", last_model_path+'.zip')
+
+def make_dir(dir_path):
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+
 def choose_device(device_name):
     if device_name == "gpu":
         device_name = "/gpu:0"
@@ -78,16 +108,21 @@ def choose_device(device_name):
 
 def get_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--device', help='CPU or GPU', default='cpu', type=str, choices=['cpu', 'gpu'])
+    parser.add_argument('--seed', help='Random generator seed', type=int, default=0)
     parser.add_argument('--env', help='environment ID', type=str, default='CartPole-v1', choices=env_list)
     parser.add_argument('--algo', help='RL Algorithm', default='trpo', type=str, required=False, choices=list(algo_list.keys()))
     parser.add_argument('--exp-id', help='Experiment ID', default=0, type=int)
-    parser.add_argument('--seed', help='Random generator seed', type=int, default=0)
 
     parser.add_argument('-rl', '--train-RL', help='To train RL', action='store_true')
     parser.add_argument('-trl', '--timesteps-RL', help='Number of timesteps for RL', default="1e5", type=str)
     parser.add_argument('-il', '--train-IL', help='To train GAIL', action='store_true')
     parser.add_argument('-til', '--timesteps-IL', help='Number of timesteps for GAIL', default="1e6", type=str)
-
+    parser.add_argument('-best', '--save-best-model', help='For saving best model from EvalCallback instead of last available model', action='store_true')
+    
+    parser.add_argument('-check', '--check-callback', help='For saving models every save_freq steps', action='store_true')
+    parser.add_argument('-eval', '--eval-callback', help='For evaluating model every eval_freq steps', action='store_true')
+    parser.add_argument('-tb', '--tensorboard', help='For Tensorboard logging', action='store_true')
     parser.add_argument('-params-RL', '--hyperparams-RL', type=str, nargs='+', default={}, action=StoreDict, help='Overwrite hyperparameter (e.g. gamma:0.95 timesteps_per_batch: 2048)')
     parser.add_argument('-params-IL', '--hyperparams-IL', type=str, nargs='+', default={}, action=StoreDict, help='Overwrite hyperparameter (e.g. gamma:0.95 timesteps_per_batch: 2048)')
     args = parser.parse_args()
@@ -97,53 +132,91 @@ def main():
 
     # Initialize env (for vectorized env, use DummyVecEnv([make_env(env_id, 0, args.seed)]))
     args = get_args()
+    set_global_seeds(args.seed)
+    choose_device(args.device)
     env_id, algo, exp_id = args.env, args.algo, args.exp_id
+    env_index = env_list.index(env_id) # for callbacks
     env_name = env_id[:-3]
     env = gym.make(env_id)
     env.seed(args.seed)
-    set_global_seeds(args.seed)
 
-    folder_RL = [env_name.lower(), args.algo.lower(), exp_id]
-    folder_IL = [env_name.lower(), 'gail', exp_id]
+    folder = [exp_id, env_name.lower(), args.algo.lower()]
     policy_RL = 'MlpPolicy'
     policy_IL = 'MlpPolicy'
-    eval_episodes = 100 #evaluation episodes for trained model
+    eval_episodes = 100 # evaluation episodes for trained RL/IL model
+    save_freq_RL, eval_freq_RL, save_freq_IL, eval_freq_IL, save_best_model, tensorboard_path = 0, 0, 0, 0, False, None # default tensorboard, callback values
+
+    # checking arguments for correctness
+    if not ((args.train_RL) or (args.train_IL)):
+        print("This program is for training an RL/IL algorithm. PLease specify at least one of the options...")
+        sys.exit()
+
+    # Adding Tensorboard features
+    if args.tensorboard:
+        tensorboard_path = "tensorboard/{}_{}".format(exp_id, env_name.lower())
+        make_dir(tensorboard_path)
+
+    # Adding callback features
+    callback = []
+    if (args.eval_callback and args.save_best_model):
+        save_best_model = True
+
+    ############  BODY OF THE CODE: RL -> EXPERT -> GAIL  ##############
 
     # Train RL algo on env
     if args.train_RL:
-        train(exp_id, env_id, env, algo, policy_RL, args.seed, timesteps=int(float(args.timesteps_RL)), hyperparams=args.hyperparams_RL)
-    
-        # Generate expert trajectories
-        expert_traj_gen = 100
-        model = (algo_list[algo]).load("models/{}_{}_{}".format(*folder_RL))
-        model.set_env(env)
-        generate_expert_traj(model, env=env, save_path='experts/{}_{}_{}'.format(*folder_RL), n_episodes=expert_traj_gen) #comment lines 172, 173 in function to not print expert data info
-
-    # Evaluate RL model
-
-    if os.path.exists("models/{}_{}_{}.zip".format(*folder_RL)):
-        model = (algo_list[algo]).load("models/{}_{}_{}".format(*folder_RL))
+        save_freq_RL, eval_freq_RL = 100*episode_len[env_index], 100*episode_len[env_index]
+        if args.check_callback:
+            check_callback_path = add_callback_path('RL', 'checkpoints', folder)
+            callback.append(CheckpointCallback(save_freq=save_freq_RL, save_path=check_callback_path, name_prefix='rl_model', verbose=1))
+        if args.eval_callback:
+            eval_callback_path = add_callback_path('RL', 'evaluations', folder)
+            callback.append(EvalCallback(env, best_model_save_path=eval_callback_path, log_path=eval_callback_path, eval_freq=eval_freq_RL, verbose=1))
+        train('RL', save_best_model, folder, env, algo, policy_RL, args.seed, timesteps=int(float(args.timesteps_RL)), 
+            tensorboard_log=tensorboard_path, callback = callback, hyperparams=args.hyperparams_RL)
+        if (args.eval_callback and args.save_best_model):
+            print('\n\nChoosing best saved model throughout training, instead of model available at end of training (EvalCallback enabled!)')
+            copy_best_model('RL', folder)
+        # Evaluate RL model
+        model = (algo_list[algo]).load("models/{}_{}_{}".format(*folder))
         evaluate('policy', model, env_id, env, algo, eval_episodes)
+
+    # Generate expert trajectories
+    if os.path.exists("models/{}_{}_{}.zip".format(*folder)):
+        expert_traj_gen = 100
+        model = (algo_list[algo]).load("models/{}_{}_{}".format(*folder))
+        model.set_env(env)
+        generate_expert_traj(model, env=env, save_path='experts/{}_{}_{}'.format(*folder), n_episodes=expert_traj_gen) #comment lines 172, 173 in function to not print expert data info
     else:
-        print('RL model unavailable. Please train...')
+        print("Trained RL model unavailable. Please train again...")
 
     #  Load expert dataset
-    if os.path.exists('experts/{}_{}_{}.npz'.format(*folder_RL)):
-        expert_traj_use = 10 # using only 10 trajectories
-        dataset = ExpertDataset(expert_path='experts/{}_{}_{}'.format(*folder_RL), traj_limitation=expert_traj_use, verbose=0)
-        expert_data = np.load('{}_{}_{}.npz'.format(*folder_RL), allow_pickle =True)
-        evaluate('expert', expert_data, env_id, env, algo, expert_traj_use)
+    if os.path.exists('experts/{}_{}_{}.npz'.format(*folder)):
+        expert_traj_use, callback = 10, [] # using only 10 trajectories
+        dataset = ExpertDataset(expert_path='experts/{}_{}_{}.npz'.format(*folder), traj_limitation=expert_traj_use, verbose=0)
+        expert_data = np.load('experts/{}_{}_{}.npz'.format(*folder), allow_pickle=True)
+        evaluate('expert', expert_data, env_id, env, algo, expert_traj_gen)
     else:
         print('Expert data unavailable. Please generate...')
         sys.exit()
 
     # Train GAIL on expert
     if args.train_IL:
-        train(exp_id, env_id, env, 'gail', policy_IL, args.seed, timesteps=int(float(args.timesteps_IL)), hyperparams=args.hyperparams_IL, dataset=dataset)
+        save_freq_IL, eval_freq_IL = 300*episode_len[env_index], 300*episode_len[env_index]
+        if args.check_callback:
+            check_callback_path = add_callback_path('IL', 'checkpoints', folder)
+            callback.append(CheckpointCallback(save_freq=save_freq_IL, save_path=check_callback_path, name_prefix='gail_model', verbose=1))
+        if args.eval_callback:
+            eval_callback_path = add_callback_path('IL', 'evaluations', folder)
+            callback.append(EvalCallback(env, best_model_save_path=eval_callback_path, log_path=eval_callback_path, eval_freq=eval_freq_IL, verbose=1))
+        train('IL', save_best_model, folder, env, algo, policy_IL, args.seed, timesteps=int(float(args.timesteps_IL)), 
+        tensorboard_log=tensorboard_path, callback = callback, hyperparams=args.hyperparams_IL, dataset=dataset)
+        if (args.eval_callback and args.save_best_model):
+            copy_best_model('IL', folder)
 
-    # Evaluate IL model
-    if os.path.exists("models/{}_{}_{}.zip".format(*folder_IL)):
-        model = (algo_list['gail']).load("models/gail_{}_{}_{}".format(*folder_IL))
+    # Evaluate GAIL model
+    if os.path.exists("models/{}_{}_{}_gail.zip".format(*folder)):
+        model = (algo_list['gail']).load("models/{}_{}_{}_gail".format(*folder))
         evaluate('policy', model, env_id, env, algo, eval_episodes)
     else:
         print('GAIL model unavailable. Please train...')
